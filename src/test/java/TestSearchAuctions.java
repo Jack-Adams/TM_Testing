11d446e6a981662589dce34ed354fe7aa45b850f
Jack Adams <adamsj@assurity.local>
2019-04-15 09:11:03 +1200

    Correcting previous commit

--- src/test/java/TestSearchAuctions.java
+++ src/test/java/TestSearchAuctions.java
@@ -18,20 +18,34 @@ public class TestSearchAuctions {
 
     @BeforeTest
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Jenkins\\workspace\\chromedriver.exe");
+        System.out.println("Set chromedriver path");
+
         // Setting Chrome to headless
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("window-size=1200x600");
+        System.out.println("Set chromedriver options");
         this.driver = new ChromeDriver(options);
+        System.out.println("made new headless chromedriver");
+        System.out.println(this.driver);
         //
+
+
 
         // Leave this uncommented unless using headless.
 //        this.driver = new ChromeDriver();
+//        System.out.println("Made new chromedriver");
+//        System.out.println(this.driver);
         //
 
+
+
         wait = new WebDriverWait(driver, 20);
+        System.out.println("Set wait");
         this.auctionPage = new SearchAuctions(driver);
+        System.out.println("Reached TM sandbox website");
     }
 
     @AfterTest
@@ -43,8 +57,11 @@ public class TestSearchAuctions {
     @Test
     public void testTheSearchFunction(){
         auctionPage.searchForItem("gold");
+        System.out.println("Entered gold into search bar");
         String searchResultsPageText = wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//*[@id='FiltersContainer']/h1"))).getText();
+        System.out.println("Located element");
         Assert.assertEquals(searchResultsPageText, "Search results for 'gold'");
+        System.out.println("Completed assertion");
     }
 }
